1be1613a22366db23682540ae4bcdca1b428668f
shen <shen@xytc.tw>
2019-09-25 18:54:57 +0800

    增加語系說明

---
+++ upload/admin/language/zh-cn/payment/ce_pay.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']					 = 'CashEnvoy (Interswitch, Verve, Mastercard)'; 
+
+// Text
+$_['text_payment']					 = 'Payment';
+$_['text_success']					 = 'Success: You have modified CashEnvoy account details!';
+$_['text_ce_pay']				 = '<a onclick="window.open(\'https://www.cashenvoy.com\');"><img src="view/image/payment/cashenvoy.png" alt="CashEnvoy" title="CashEnvoy" style="border: 1px solid #EEEEEE;" /></a>';
+
+// Entry
+$_['entry_mertid']					 = 'Merchant Id:';
+$_['entry_test']					 = 'Sandbox Mode:';
+$_['entry_total']                    = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
+$_['entry_completed_status']         = 'Completed Status:';
+$_['entry_denied_status']			 = 'Denied Status:';
+$_['entry_failed_status']			 = 'Failed Status:';
+$_['entry_geo_zone']				 = 'Geo Zone:';
+$_['entry_status']					 = 'Status:';
+$_['entry_sort_order']				 = 'Sort Order:';
+
+// Error
+$_['error_permission']				 = 'Warning: You do not have permission to modify payment CashEnvoy!';
+$_['error_mertid']					 = 'Merchant Id is required! Must be a number.';
+$_['error_total']					 = 'Total cannot be less than 50.';
+?>
---
+++ upload/catalog/language/zh-hk/payment/ce_pay.php
@@ -0,0 +1,8 @@
+<?php
+// Text
+$_['text_title']    = '<b><img src="admin/view/image/payment/cashenvoy.png" align="absmiddle" alt="CashEnvoy" title="CashEnvoy"/> (Interswitch, Verve, Mastercard)</b>';
+$_['order_failure']    = '<p style="padding-left:20px;">Your Order was not processed!</p>';
+$_['text_reason'] 	= 'REASON';
+$_['text_testmode']	= 'Warning: The Payment Gateway is in \'Sandbox Mode\'. Your account will not be charged.';
+$_['text_total']	= 'Shipping, Handling, Discounts & Taxes';
+?>
